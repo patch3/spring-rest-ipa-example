76a45427c2a6c630dd44167ace5a9f502022da97
patch3 <89272995+patch3@users.noreply.github.com>
2024-03-06 16:41:27 +0300

    Delete spring-rest-ipa-server directory

--- spring-rest-ipa-server/src/main/java/org/example/springrestipaserver/controllers/ClientController.java
+++
@@ -1,8 +0,0 @@
-package org.example.springrestipaserver.controllers;
-
-import org.example.springrestipaserver.repository.ClientRepository;
-import org.springframework.http.client.observation.ClientRequestObservationConvention;
-
-public class ClientController implements ClientRepository {
-
-}
